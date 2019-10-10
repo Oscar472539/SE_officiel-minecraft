5e502336eb60f409ce0f746a89f46ad8c50812fe
Oscar472539 <55753258+Oscar472539@users.noreply.github.com>
2019-10-10 13:22:49 +0200

    Add files via upload

---
+++ HTML/myscripts.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("min_burger_menu").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("min_burger_menu").style.width = "0";
+  }
